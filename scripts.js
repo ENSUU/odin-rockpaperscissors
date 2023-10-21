1e4ca500ab62b0229e6ea86e38deb81b328dd509
ENSUU <eunsu.dk.kim@gmail.com>
2023-10-20 21:58:10 -0400

    Add a "Play Again" button to restart the game
    Once either the player or CPU's score reaches 5, a "Play Again"
    button will appear on the screen to reset the game. Allows the game
    to have some replay value.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+// Generate the computer's choice randomly.
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"]; 
     return choices[Math.floor(Math.random() * choices.length)]
 }
 
+// Handles the logic of who won the round based on rock-paper-scissor rules.
 function playRound(playerSelection, computerSelection, scores) {
 
     const roundResults = document.querySelector('body > div.container > div.roundResult > h2'); 
@@ -49,33 +51,50 @@ function playRound(playerSelection, computerSelection, scores) {
     return scores; 
 }
 
+// Starts the game up. 
 function game() {
-
+    // Initializing a score of 0 for both the player and CPU.
     let scores = {
         'Player': 0, 
         'Computer': 0
     };
 
+    // Selecting the DOM elements for the options, player score, and CPU score.
     const buttons = document.querySelectorAll('button'); 
     const playerScore = document.querySelector('.playerScore h3'); 
     const computerScore = document.querySelector('.computerScore h3');
 
+    // For each button option, add a click event listener which calls playRound function when clicked.
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             scores = playRound(button.innerText, getComputerChoice(), scores);
             playerScore.innerText = scores['Player']; 
             computerScore.innerText = scores['Computer'];
 
+            // When either the player or CPU reaches a score of 5, stop the game. 
             if (scores['Player'] === 5 || scores['Computer'] === 5) {
                 const controls = document.querySelector('.controls'); 
                 controls.remove(); 
-        
+                
+                // Editing the text under the options to display the winner. 
                 const roundResults = document.querySelector('body > div.container > div.roundResult > h2'); 
                 let winner = scores['Player'] === 5 ? 'Player' : 'CPU'; 
                 roundResults.innerText = `${winner} wins the game!`; 
 
                 const roundWinner = document.querySelector("body > div.container > div.roundResult > h3");
                 roundWinner.innerText = "";
+                
+                // Adding a "Play Again" button once the score reaches 5 for either the CPU or Player.
+                const resetButton = document.createElement('button'); 
+                resetButton.textContent = 'Play Again';
+                resetButton.style.cssText = "padding: 1rem 0;";
+                resetButton.style.fontWeight = "bold";
+
+                resetButton.addEventListener('click', () => {
+                    location.reload(); 
+                })
+
+                document.querySelector('.container').appendChild(resetButton); 
             }
         })
     }) 
